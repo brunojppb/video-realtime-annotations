104d5e2a1bff0e8f930ff0b7c814a9a0f00cf867
Bruno Paulino <bruno@woombikes.com>
2020-06-03 22:34:45 +0200

    create my own authentication plug and add video model with user based access

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,7 +4,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -25,7 +25,7 @@ defmodule RumblWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
-        |> Rumbl.AuthWeb.login(user)
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created")
         |> redirect(to: Routes.user_path(conn, :index))
 
@@ -33,17 +33,5 @@ defmodule RumblWeb.UserController do
         render(conn, "new.html", changeset: changeset)
     end
   end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
-    end
-  end
-
 
 end
